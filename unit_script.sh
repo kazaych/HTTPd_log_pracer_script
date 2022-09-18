dba07f42040a52ed4e803ecec7a96c3825ec39de
anatolijcurikov <kazay@mail.ru>
2022-09-18 18:13:17 +0300

    valid_time func edited

--- unit_script.sh
+++ unit_script.sh
@@ -4,7 +4,7 @@
 
 function clean {
         echo "Deleting temp files"
-        rm -f tmp.log && rm -f /var/lock/unit_script.lock && rm -f last_launch.log
+        rm -f tmp.log /var/lock/unit_script.lock last_launch.log
         echo "Success"
 }
 
@@ -26,15 +26,33 @@ if test -r last_launch.log; then
 	echo Script was previously run at `cat last_launch.log | sed s'/\[//'`
 
 else
-	echo [14/Aug/2019:00:00:00 > last_launch.log
+	echo 14/Aug/2019:00:00:00 > last_launch.log
 fi
 
 LOG=$1
 
 # Section 4. Parse time range between time in last_launch.log and current time, write range of strings in time range to tmp.log file
+#function valid_time {
+#	touch tmp.log
+#	awk -vSTART_Date=`cat last_launch.log` -vEND_Date=`date -d 'now' +[%d/%b/%Y:%H:%M:%S` '$4 > START_Date && $4 < END_Date {print $0}' $1 > tmp.log
+#}
+
 function valid_time {
-	touch tmp.log
-	awk -vSTART_Date=`cat last_launch.log` -vEND_Date=`date -d 'now' +[%d/%b/%Y:%H:%M:%S` '$4 > START_Date && $4 < END_Date {print $0}' $1 > tmp.log
+    tac access.log > access.tmp.log
+    time_start=$(cat last_launch.log | sed s'/\// /g' | sed s'/\:/ /' | awk '{print $2" "$1" "$3" "$4}')
+    time_start_sec=$(date --date="$time_start" +"%s")
+
+    while IFS= read -r line; do
+            time=$(echo $line |awk '{print $4}' | sed s'/\[//' | sed s'/\// /g' | sed s'/\:/ /' | awk '{print $2" "$1" "$3" "$4}')
+            log_time=$(date --date="$time" +"%s")
+	    if [ $log_time -gt $time_start_sec ]; then
+		    echo $line >> tmp.log
+	    else
+		    break
+	    fi
+    done < access.tmp.log
+rm -f access.tmp.log
+
 }
 
 # Section 5. Parse 3 ip addresses from access.log after cut range of time with valid_time
@@ -74,7 +92,7 @@ EOF
 else
 cat <<-EOF > result.log
 	--------------------------------------------------------------
-	This log create from `cat last_launch.log | sed 's/\[//'` to `date -d'now' +%d/%b/%Y:%H:%M:%S`
+	This log create from `cat last_launch.log | sed 's/\[//'` to `date -d 'now' +%d/%b/%Y:%H:%M:%S`
 	--------------------------------------------------------------
 	------ MAXIUMUM IP COUNT ------
 	$(best_ip)
@@ -90,7 +108,8 @@ fi
 
 # Section 11. Delete environment and write last run time
 rm -f /var/lock/unit_script.lock
-date +"[%d/%b/%Y:%H:%M:%S" > last_launch.log
+
+date +"%d/%b/%Y:%H:%M:%S" > last_launch.log
 
 # Section 12. Create and send email
 mail -a result.log -s "Log file from httpd" kazay@mail.ru < /dev/null
